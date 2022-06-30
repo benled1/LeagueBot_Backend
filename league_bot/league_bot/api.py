cb74925b6fbe00bbc97167193f940cc8d37a9627
Ben Ledingham <benledingham1@gmail.com>
2022-06-30 10:32:08 -0700

    new comments on how api should work

--- league_bot/league_bot/api.py
+++ league_bot/league_bot/api.py
@@ -15,12 +15,17 @@ def add(request, a: int, b:int):
 
 @api.get("/champion_stats")
 def champion_stats(request, champ_name: str):
+    """
+    TRY DOING THIS USING AN S3 BUCKET TO STORE ALL THE IMAGES. GENERATE_IMAGES SHOULD OVERWRITE THE IMAGES IN S3 BUCKET
+    A CALL TO THIS FUNCTION SHOULD PROVIDE LINKS TO ALL THE IMAGE LOCATIONS SO THEY CAN BE DOWNLOADED TEMPORARILY
+    ONTO THE FRONTEND AND SENT TO THE DISCORD CHANNEL AS AN EMBED. 
+    """
     # champion_row = Champion.objects.filter(champ_name=champ_name)
-    with open(f'/Users/benledingham/Documents/League Bot/League_App/league_bot/league_bot/final_images/champ_stat_cards/Annie.png', 'rb') as imageFile:
-        encoded_champ_img = base64.b64encode(imageFile.read()).decode('utf-8')
-    json_img = json.dumps([{'image': encoded_champ_img}])
+    with open(f'/workspace/league_bot/league_bot/final_images/champ_stat_cards/Annie.png', 'rb') as image_file:
+        encoded_champ_img = str(base64.b64encode(image_file.read()))
+        # json_img = json.dumps([{'image': encoded_champ_img}])
     # champion_row_json = serializers.serialize('json', champion_row)
-    return json_img
+    return encoded_champ_img
 
 
 
