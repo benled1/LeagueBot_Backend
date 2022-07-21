9d554949d3f7ff82755bb7bfe47e469ad833d3d0
Ben Ledingham <benledingham1@gmail.com>
2022-07-20 19:52:56 -0700

    update_stats updates the champion builds table

--- league_bot/stat_functions/champion.py
+++ league_bot/stat_functions/champion.py
@@ -1,5 +1,9 @@
+from distutils.command.build import build
+import pandas as pd
+
 from league_bot.models import Participant
-from django.db.models import Count
+from django.db.models import Count, Sum
+
 
 def get_winrate(all_champ_part_records):
     number_of_records = all_champ_part_records.count()
@@ -17,8 +21,43 @@ def get_play_count(all_champ_part_records):
 
 
 def get_best_builds(champion_name):
-    best_build_query_set = (Participant.objects
-    .values('item0', 'item1', 'item2', 'item3', 'item4', 'item5')
+    win_build_counts = (Participant.objects
+    .filter(champ_name=champion_name)
+    .filter(win=True)
+    .values('champ_name', 'item0', 'item1', 'item2', 'item3', 'item4', 'item5')
     .annotate(build_count=Count("id"))
     .order_by("-build_count"))
-    print(best_build_query_set)
+
+    total_build_counts = (Participant.objects
+    .filter(champ_name=champion_name)
+    .values('champ_name', 'item0', 'item1', 'item2', 'item3', 'item4', 'item5')
+    .annotate(build_count=Count("id"))
+    .order_by("-build_count"))
+
+    total_df = pd.DataFrame(total_build_counts)
+    win_df = pd.DataFrame(win_build_counts)
+    if len(total_df) != 0:
+        build_df = pd.merge(
+                total_df, win_df,  how='left',
+                left_on=['champ_name', 'item0', 'item1', 'item2', 'item3', 'item4', 'item5'],
+                right_on = ['champ_name', 'item0', 'item1', 'item2', 'item3', 'item4', 'item5']
+                )
+        build_df.rename(columns={'build_count_x': 'num_plays',
+                                'build_count_y': 'num_wins'}, inplace=True)
+        build_df.fillna(value=0.0, inplace=True)
+        build_df['win_rate'] = build_df['num_wins']/build_df['num_plays']
+        build_df.sort_values(by='win_rate', ascending=False, inplace=True)
+    else:
+        return "No Builds"
+    return build_df
+
+    # print(f"{len(total_build_counts)} builds for {champion_name}")
+    # for build in total_build_counts:
+    #     if build not in win_build_counts:
+    #         build['build_winrate'] = 0.0
+        
+    #     win_build_index = win_build_counts
+        
+
+
+
