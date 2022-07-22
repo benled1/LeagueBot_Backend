6c892d425179850324330be93ad4d2edcba3b2b6
Ben Ledingham <benledingham1@gmail.com>
2022-07-22 16:45:15 -0700

    creating func to overlay item imgs onto stat cards

--- league_bot/image_functions/champ_stat_cards.py
+++ league_bot/image_functions/champ_stat_cards.py
@@ -2,16 +2,21 @@ import boto3
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+"""
+For next time:
+
+Continue working on adding the item images to the champ_stat_card:
+
+- This will require that you download and save the images from s3 to
+    a tmp folder. These tmp folders are destroyed when the dyno is re-run since they are 
+    ignored in the git folder.
+- Once the images are downloaded and stored in tmp folder, load them into PIL and 
+    overlay them on the champ_stat_card how you want
+
+"""
 
 
-def get_champion_stat_card(champ_row):
-    champ_winrate = round(champ_row['champ_winrate'] * 100, 2)
-    champ_play_count = champ_row['champ_play_count']
-
-    s3_resource = boto3.resource('s3')
-
-    stat_card = Image.open("league_bot/static_images/backdrop.jpeg")
-    print(stat_card.size)
+def add_winrate_playcount(stat_card, champ_winrate, champ_play_count):
     draw = ImageDraw.Draw(stat_card)
     number_font = ImageFont.truetype("league_bot/fonts/Friz_Quadrata_Regular.ttf", 100)
     label_font = ImageFont.truetype("league_bot/fonts/Friz_Quadrata_Bold.otf", 25)
@@ -22,6 +27,20 @@ def get_champion_stat_card(champ_row):
     # Draw in champ_playcount and text
     draw.text((20, 300),f"{champ_play_count}",(115,91, 48),font=number_font)
     draw.text((20, 400),f"MATCHES",(115,91, 48),font=label_font)
+    return stat_card
+    pass
+
+
+
+def get_champion_stat_card(champ_row):
+    champ_winrate = round(champ_row['champ_winrate'] * 100, 2)
+    champ_play_count = champ_row['champ_play_count']
+
+    s3_resource = boto3.resource('s3')
+
+    stat_card = Image.open("league_bot/static_images/backdrop.jpeg")
+    stat_card = add_winrate_playcount(stat_card=stat_card, champ_winrate=champ_winrate, champ_play_count=champ_play_count)
+    
     
     if champ_row['champ_name'] != 'MonkeyKing':
         champ_name = champ_row['champ_name']
@@ -30,3 +49,4 @@ def get_champion_stat_card(champ_row):
     stat_card.save(f"league_bot/tmp_images/champ_stat_cards/{champ_name}.png", "PNG")
 
     return champ_name
+
